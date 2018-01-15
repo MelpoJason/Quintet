a163401faba64022582efeb11ce37fa0a77fe43d
Jason <jasonchan9280@gmail.com>
2018-01-15 11:04:29 +0800

    add actuator to the project

--- backend/src/main/java/com/quintet/BackendApplication.java
+++ backend/src/main/java/com/quintet/BackendApplication.java
@@ -3,7 +3,6 @@ package com.quintet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 //@EnableCaching
