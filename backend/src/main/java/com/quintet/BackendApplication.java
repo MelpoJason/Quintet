d98d940e28450e9527d6bf36489ce1019b15ae98
Jason <jasonchan9280@gmail.com>
2018-01-30 18:00:35 +0800

    add swagger to the project

--- backend/src/main/java/com/quintet/BackendApplication.java
+++ backend/src/main/java/com/quintet/BackendApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-//@EnableCaching
 public class BackendApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
--- dataMining/dataMining/items.py
+++ dataMining/dataMining/items.py
@@ -9,6 +9,6 @@ import scrapy
 
 
 class DataminingItem(scrapy.Item):
+    pass
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
