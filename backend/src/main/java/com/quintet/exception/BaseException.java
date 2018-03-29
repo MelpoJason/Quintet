7fbf4fde2d8156788814cadbfe9ef6d12649e42d
Jason <jasonchan9280@gmail.com>
2018-03-29 11:02:26 +0800

    添加fastjson、commons-io、commons-codec， 设置异常抛出规范

--- backend/src/main/java/com/quintet/exception/BaseException.java
+++ backend/src/main/java/com/quintet/exception/BaseException.java
@@ -1,6 +1,6 @@
 package com.quintet.exception;
 
-public class BaseException extends RuntimeException {
+abstract public class BaseException extends RuntimeException {
     private static final long serialVersionUID = -7619231466939296677L;
 
     static final String INVALID_PARAMS = "INVALID_PARAMS";
