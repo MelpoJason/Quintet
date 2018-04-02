f785526885085614f5d4145ae640764de673373b
Jason <jasonchan9280@gmail.com>
2018-04-02 16:12:17 +0800

    配置swagger

---
+++ backend/src/main/java/com/quintet/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.quintet.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket createRestDocs() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.quintet.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("quintet Api手册")
+                .description("quintet相关Restful API")
+                .contact(new Contact("谌卓", "", "jasonchan9280@gmail.com"))
+                .version("1.0")
+                .build();
+    }
+}
